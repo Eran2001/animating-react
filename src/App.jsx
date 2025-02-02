0f8de1cb733fbda0e383710d06b54b42e1c81c28
Eran <eran.hasarali.2001@gmail.com>
2025-02-02 11:07:39 +0530

    add some animations using springs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useSpring, animated } from "@react-spring/web";
+
 const App = () => {
+  const fade = useSpring({
+    from: {
+      x: -100,
+      opacity: 0,
+    },
+    to: {
+      x: 50,
+      opacity: 1,
+    },
+    delay: 1000,
+  });
+
+  const lateFade = useSpring({
+    from: {
+      x: -100,
+    },
+    to: {
+      x: 50,
+    },
+    delay: 2000,
+  });
+
+  const styles = useSpring({
+    marginTop: "-0.5rem",
+  });
+
   return (
     <div>
-      <h1>Hello</h1>
+      <animated.h1 style={{ ...fade }}>Hello</animated.h1>
+      <animated.h2 style={{ ...lateFade, ...styles }}>World</animated.h2>
     </div>
   );
 };
